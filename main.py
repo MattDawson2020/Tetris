df20f751b7a35458dc8d75bc0f557d9dedc13de4
matt.dawson@artlogic.net <matt.dawson@artlogic.net>
2025-02-10 09:53:18 +0000

    Initial commit, can see screen and inputs

---
+++ main.py
@@ -0,0 +1,115 @@
+import sys
+import pygame
+import random
+
+pygame.init()
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+COLOURS = [RED, BLUE, GREEN]
+
+# Tetris shapes
+SHAPES = [
+    [
+        ['.....',
+         '.....',
+         '.....',
+         'OOOO.',
+         '.....'],
+        ['.....',
+         '..O..',
+         '..O..',
+         '..O..',
+         '..O..']
+    ],
+    [
+        ['.....',
+         '.....',
+         '..O..',
+         '.OOO.',
+         '.....'],
+        ['.....',
+         '..O..',
+         '.OO..',
+         '..O..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.OOO.',
+         '..O..',
+         '.....'],
+        ['.....',
+         '..O..',
+         '..OO.',
+         '..O..',
+         '.....']
+    ],
+    [
+        [
+         '.....',
+         '.....',
+         '..OO.',
+         '.OO..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.OO..',
+         '..OO.',
+         '.....'],
+        ['.....',
+         '.O...',
+         '.OO..',
+         '..O..',
+         '.....'],
+        ['.....',
+         '..O..',
+         '.OO..',
+         '.O...',
+         '.....']
+    ],
+    [
+        ['.....',
+         '..O..',
+         '..O.',
+         '..OO.',
+         '.....'],
+        ['.....',
+         '...O.',
+         '.OOO.',
+         '.....',
+         '.....'],
+        ['.....',
+         '.OO..',
+         '..O..',
+         '..O..',
+         '.....'],
+        ['.....',
+         '.....',
+         '.OOO.',
+         '.O...',
+         '.....']
+    ],
+]
+
+
+def main():
+    # initialise the game
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Tetris")
+    clock = pygame.time.Clock()
+
+    while True:
+        screen.fill(BLACK)
+        for event in pygame.event.get():
+            print(event)
+
+if __name__ == "__main__":
+    main()
